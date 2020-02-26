84836882702176f44766f82ead6a095b076cf585
Rami Mounla <Rami_Mounla@hotmail.com>
2020-02-26 23:06:00 +1300

    Added notify changes method

--- PcfNotify/index.ts
+++ PcfNotify/index.ts
@@ -1,13 +1,15 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
 export class PcfNotify implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
 	private _inputElement: HTMLInputElement;
+	private _notifyOutputChanged: () => void;
+	private _inputChanged: EventListenerOrEventListenerObject;
+
 	/**
 	 * Empty constructor.
 	 */
-	constructor()
-	{
+	constructor() {
 
 	}
 
@@ -19,13 +21,16 @@ export class PcfNotify implements ComponentFramework.StandardControl<IInputs, IO
 	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
 	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
 	 */
-	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
-	{
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
 		// Add control initialization code
 		this._inputElement = document.createElement("input");
 		this._inputElement.value = context.parameters.inputProperty.raw || "";
-
 		container.appendChild(this._inputElement);
+
+		this._notifyOutputChanged = notifyOutputChanged;
+		this._inputChanged = this.inputChanged.bind(this)
+
+		this._inputElement.addEventListener("change", this._inputChanged);
 	}
 
 
@@ -33,27 +38,29 @@ export class PcfNotify implements ComponentFramework.StandardControl<IInputs, IO
 	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
 	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
 	 */
-	public updateView(context: ComponentFramework.Context<IInputs>): void
-	{
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
 		// Add code to update control view
 		this._inputElement.value = context.parameters.inputProperty.raw || "";
+		this._notifyOutputChanged();
 	}
 
 	/** 
 	 * It is called by the framework prior to a control receiving new data. 
 	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
 	 */
-	public getOutputs(): IOutputs
-	{
+	public getOutputs(): IOutputs {
 		return {};
+	}
+
+	public inputChanged(evt: Event): void {
+		this._notifyOutputChanged();
 	}
 
 	/** 
 	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
 	 * i.e. cancelling any pending remote calls, removing listeners, etc.
 	 */
-	public destroy(): void
-	{
+	public destroy(): void {
 		// Add code to cleanup control if necessary
 	}
 }
